1404e77d611054dc9d8d5a2fcf6171116fd3bace
user5 <vfedorovich@yandex.ru>
2020-02-25 16:00:46 +0200

    20200225_16_00

---
+++ lab4/history.m
@@ -0,0 +1,31 @@
+%-- 02/25/2020 02:41:55 PM --%
+lab3Demo
+zeros(2,4)
+size(A)
+A = zeros(2,4)
+size(A)
+rand(2,4)
+t1 = [1 2 3];
+t2 = [4 5];
+t = [t1 t2]
+lab3Demo
+%% datu filtrācija
+t = 0:6
+2>5
+2<5
+t = 0:6
+tf = t>3
+t(tf)
+lab3Demo
+lab3Demo(4)
+lab3Demo(0.7)
+lab3Demo([0.7 0 4])
+t = 0:0.01:5;
+y = lab3_demo(t);
+plot(t,y)
+lab3_demo;
+lab3_demo(4);
+lab3_demo([4 5]);
+lab3_demo([4:00.1:5]);
+y = lab3_demo([4:00.1:5]);
+y = lab3_demo([4:00.1:5])
